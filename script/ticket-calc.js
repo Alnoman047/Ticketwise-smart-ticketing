97ac6d1dd3b6eb55a4247151552b31c9f6729e1b
Noman <alnoman047@gmail.com>
2024-02-19 02:48:19 +0600

    added buy button functionality

---
+++ script/ticket-calc.js
@@ -0,0 +1,12 @@
+// scroll down to ticket purchasing page
+
+function buyTicket(buyTickets, ticketPurchase){
+    const ticketSection = document.getElementById('ticketPurchase');
+    ticketSection.scrollIntoView({behavior: 'smooth'});
+}
+
+// change seat availability
+
+
+
+
